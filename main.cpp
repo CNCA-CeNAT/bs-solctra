b7d43ff847d215d2f87d8beae56fc1e921b8f004
LuisDiego Chavarria <ldcl289@gmail.com>
2016-04-18 22:07:04 -0600

    Doing some reformating. Removing the file that is going to be written before start (clean it up).

--- main.cpp
+++ main.cpp
@@ -1,61 +1,62 @@
 
 #include "solctra.h"
+#include <cstdlib>
+#include <string>
+#include <iostream>
 
-int main()
+const unsigned DEFAULT_STEPS = 500000;
+const float DEFAULT_STEP_SIZE = 0.001;
+const unsigned DEFAULT_PRECISION = 5;
+
+unsigned getPrintPrecisionFromArgs(const int& argc, char** argv)
 {
-    float spoint[3];
-    cartesian A={0,0,0},B;          // A:start point of field line
-    int i;                          // i:Número de líne en calculo
-    int mode;
+    for(unsigned i = 1 ; i < argc - 1 ; ++i)
+    {
+        std::string tmp = argv[i];
+        if(tmp == "-precision")
+        {
+            return static_cast<unsigned>(atoi(argv[i+1]));
+        }
+    }
+    return DEFAULT_PRECISION;
+}
+unsigned getStepsFromArgs(const int& argc, char** argv)
+{
+    for(unsigned i = 1 ; i < argc - 1 ; ++i)
+    {
+        std::string tmp = argv[i];
+        if(tmp == "-steps")
+        {
+            return static_cast<unsigned>(atoi(argv[i+1]));
+        }
+    }
+    return DEFAULT_STEPS;
+}
+float getStepSizeFromArgs(const int& argc, char** argv)
+{
+    for(unsigned i = 1 ; i < argc - 1 ; ++i)
+    {
+        std::string tmp = argv[i];
+        if(tmp == "-stepSize")
+        {
+            return strtod(argv[i+1], nullptr);
+        }
+    }
+    return DEFAULT_STEP_SIZE;
+}
 
-    mode=1;
+int main(int argc, char** argv)
+{
+    cartesian A={0,0,0};          // A:start point of field line
     load_coil_data();
     e_roof();
-
-
-
-
-    /* for (i=46;i<47;i=i++){
-         A.x=(1.982197e-01)+0.00474228/2*i;
-         //A.z=-0.02111;
-         //A.x=(0.2381-0.0944165)+0.00474228/2*40;
-         //A.x=1.87451e-01+0.00474228/2*i;//0.19775;//+0.00474228/2*i;A.x=1.87451e-01+0.00474228/2*i;
-         RK4(A,500000,0.001,i,1);
-
-     }*/
-
     A.x=2.284e-01;
     A.z=-0.0295;
-    //A.x=(0.2381-0.0944165)+0.00474228/2*40;
-    //A.x=1.87451e-01+0.00474228/2*i;//0.19775;//+0.00474228/2*i;A.x=1.87451e-01+0.00474228/2*i;
-    RK4(A,500000,0.001,5,1);
-
-
-
-/*
-    return 0;
-  ///////
-
-    float spoint[3];
-    cartesian A={0,0,0},B;          // A:start point of field line
-    int i;                          // i:Número de líne en calculo
-    int b;
-
-
-    load_coil_data();
-
-    e_roof();
-
-    for (i=0;i<30;i++){
-
-        //A.z=-0.02111;
-
-        A.x=1.87451e-01+0.00474228/2*i;
-        //A.x=1.87451e-01+0.00474228/2*i;//0.19775;//+0.00474228/2*i;A.x=1.87451e-01+0.00474228/2*i;
-
-      RK4(A,1000000,0.001,i);
-
-    }
-*/
+    const unsigned steps = getStepsFromArgs(argc, argv);
+    const float stepSize = getStepSizeFromArgs(argc, argv);
+    const unsigned precision = getPrintPrecisionFromArgs(argc, argv);
+    std::cout.precision(precision);
+    std::cout << "Running with steps=[" << steps << "] and step size=[" << stepSize << "]." << std::endl;
+    RK4(A, steps, stepSize, 5, 1);
     return (5);
 }
--- solctra.h
+++ solctra.h
@@ -9,27 +9,24 @@
 #define     miu     1.2566e-06
 #define     I       -4350
 
-typedef     struct{
-
+typedef struct
+{
     float x, y, z;
+} cartesian;
 
-}cartesian;
-
-struct  coil{
-
+struct coil
+{
     float x[361], y[361], z[361];
-
 };
-extern struct coil num_coil[12],vec_e_roof[12],Rmi,Rmf;
-extern float   leng_segment[12][360];
+extern struct coil num_coil[12], vec_e_roof[12], Rmi, Rmf;
+extern float leng_segment[12][360];
 
-void    e_roof(void);
-void    load_coil_data(void);
-cartesian load_path(int point);
-cartesian     magnetic_field(cartesian point);
-float norm_of(cartesian vec);
-void    R_vectors(float xx, float yy, float zz, int act_coil);
-void    RK4(cartesian start_point,int steps,float step_size, int path,int mode);
-int save_data(int phat_num,float x,float y, float z);
+void e_roof();
+void load_coil_data();
+cartesian magnetic_field(const cartesian& point);
+float norm_of(const cartesian& vec);
+void R_vectors(const float& xx, const float& yy, const float& zz, const int act_coil);
+void RK4(const cartesian& start_point, const unsigned int steps, const float& step_size, const int path, const int mode);
+int save_data(const int phat_num, const float& x, const float& y, const float& z);
 
 #endif //SOLCTRA_SOLCTRA_H
